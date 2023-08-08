db88f6c32fbae1dd93502f195da47beb84266ae7
jpthedev <jaya.prakash@flyerssoft.com>
2023-08-08 14:46:02 +0530

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const reducer = (state, Action ) => {
+  switch (Action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1, showText: state.showText };
+    case 'SHOWTEXT':
+      return {count : state.count , showText : !state.showtext}
+  }
+}
 
-  const triggerCounter = () => setCount(prevState => prevState + 1)
+function App() {
+
+  const [state, dispatch] = useReducer(reducer,{count:0 , showText:false})
+  // const triggerCounter = () => setCount(prevState => prevState + 1)
   return (
     <>
-      {count}<div>
-        <button onClick={triggerCounter}>Click here</button>
+      {state.count}<div>
+        <button onClick={()=>dispatch({type:'INCREMENT'})}>Click here</button>
       </div>
     </>
   )
